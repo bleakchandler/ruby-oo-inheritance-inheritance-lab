81c154e0011c989485852206a1b858ce7158ea16
Bleak Chandler <bleakchandler@gmail.com>
2021-02-10 10:51:33 -0500

    commit tree 5cedc0a36b05b3714c1d6d0d969867cefe7ef8ea

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,31 @@
-class Student 
+require_relative "../lib/user.rb"
+
+
+class Student < User
+
+  
+#   def initialize(fist_name, last_name)
+#     @first_name = first_name
+#     @last_name = last_name
+  
+  
+    # def first_name
+    #     @first_name
+    # end
+
+
+    # def last_name
+    #     @last_name
+    # end
+
+    def new
+        knowledge = [] 
+    end
+
+    def learn
+    end
+
+    def knowledge
+    end
 
 end
